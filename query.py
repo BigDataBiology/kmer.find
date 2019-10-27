6e5e5d4f6cd61d6706edd84c438a05f4a2c7aebb
Luis Pedro Coelho <luis@luispedro.org>
2019-10-27 02:40:53 +0200

    BUG Open the correct databasesize file

--- query.py
+++ query.py
@@ -20,17 +20,17 @@ class GetNames(object):
     def get(self, ix):
         self.headers.seek(32 * ix)
         return self.headers.readline().decode('ascii').strip()
-        
+
 Qs = {}
 for fa in skbio.io.read(query_fname, format='fasta'):
     Qs[fa.metadata['id']] = str(fa)
 
-    
+
 headers = GetNames(f'{index_base}/kmer.index/{index_fname}.names.32')
-print("Loaded headers") 
-    
+print("Loaded headers")
+
 index = IndexedFastaReader(path.join(index_base, index_fname))
-    
+
 data = subprocess.Popen(['Query',
                 '-i', query_fname,
                 '-o', '/dev/stdout',
@@ -38,11 +38,11 @@ data = subprocess.Popen(['Query',
                 '-2', f'{index_base}/kmer.index/{index_fname}.kmer.ix2'],
         stdout=subprocess.PIPE)
 
-matches = []	
+matches = []
 lamda =	 0.318                        # λ is the Gumble distribution constant
 K = 0.13                              # K is a constant associated with the scoring matrix used.
 database_size = 0
-for line in open(f'{index_base}/{index_fname}.databasesize'):
+for line in open(f'{index_base}/kmer.index/{index_fname}.databasesize'):
     database_size = database_size + int(line.strip())  #the size of the database(m)
 
 for line in chain(data.stdout, [b'END']):
@@ -52,7 +52,7 @@ for line in chain(data.stdout, [b'END']):
         if len(matches):
             matches.sort(key=lambda m: m[1]['optimal_alignment_score'], reverse=True)
             for fah, m in matches:
-                bit_score = (lamda * m.optimal_alignment_score - math.log(K)) / math.log(2)	
+                bit_score = (lamda * m.optimal_alignment_score - math.log(K)) / math.log(2)
                 p_value = math.pow(2, (-bit_score))
                 E_value = database_size * len(m.query_sequence) * p_value
                 print(f'{active}\t{fah}\t{m.optimal_alignment_score}\t{bit_score}\t{E_value}')
